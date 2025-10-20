d9f3f5f297c5571f6bc506b899df9e8d225ca64e
olakunle-remi <abbeypentious@gmail.com>
2025-10-20 10:06:47 -0500

    html and react code

---
+++ HTML-075/REACT-075/classcode/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './component/Header';
+import Box  from './component/Box';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Box />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ HTML-075/REACT-075/classcode/src/component/Box.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './box.css';
+
+function Box(){
+    return(
+<div className='wrapper'>
+    <div className='inner'>
+    <div className='div1'>
+        <h3>280+</h3>
+        <p>Google reveiw</p>
+    </div>
+    <div className='div1'>
+        <h3>280+</h3>
+        <p>Google reveiw</p>
+    </div>
+    <div className='div1'>
+        <h3>280+</h3>
+        <p>Google reveiw</p>
+    </div>
+    </div>
+</div>
+    );
+}
+export default Box;
