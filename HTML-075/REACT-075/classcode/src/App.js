e49bfd9044790a20419b88a746c33ae82e7d65a3
olakunle-remi <abbeypentious@gmail.com>
2025-10-22 17:44:39 +0100

    react code

--- HTML-075/REACT-075/classcode/src/App.js
+++ HTML-075/REACT-075/classcode/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './component/Header';
 import Box  from './component/Box';
-import './App.css';
+import UserProfile from './component/UserProfile';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Box />
+      <UserProfile />
      
     </div>
   );
